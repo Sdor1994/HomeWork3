0a3d51bc48da88edbe48fb65db7bcd3f22343a4a
semen <semen.dorohov@yandex.ru>
2023-01-28 14:55:04 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿///  Задайте массив заполненный случайными положительными трёхзначными числами. 
+///  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+void FillMas(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int num = new Random().Next(100,999);
+        arr[i] = num;
+    }
+}
+
+void PrintMas(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+   /// Console.WriteLine(arr);
+}
+
+
+Console.Write("Введите размерность массива: ");
+int k = int.Parse(Console.ReadLine()?? "");
+int[] arr = new int[k];
+FillMas(arr);
+PrintMas(arr);
+
+int Summ = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i%2==0) Summ = Summ + 1; 
+    
+    
+}
+Console.WriteLine();
+Console.WriteLine("Колличество чентых чисел = "+Summ);
+
